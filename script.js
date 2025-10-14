4541fb5bae90ca1d168a15f969a3f21a65dae5f0
BlessingLartey <bbklartey@gmail.com>
2025-10-14 09:12:17 +0000

    removed settimeout function

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ const modalBox = document.getElementById("modalBox");
 
 openBtn.addEventListener("click", () => {
   modal.classList.remove("hidden");
-  setTimeout(() => {
-    modalBox.classList.remove("scale-95", "opacity-0");
-    modalBox.classList.add("scale-100", "opacity-100");
-  }, 50);
+  modalBox.classList.remove("scale-95", "opacity-0");
+  modalBox.classList.add("scale-100", "opacity-100");
+  // setTimeout(() => {
+  //   modalBox.classList.remove("scale-95", "opacity-0");
+  //   modalBox.classList.add("scale-100", "opacity-100");
+  // }, 50);
 });
 
 closeBtn.addEventListener("click", () => {
   modalBox.classList.add("scale-95", "opacity-0");
-  setTimeout(() => modal.classList.add("hidden"), 200);
+  modal.classList.add("hidden");
+  // setTimeout(() => modal.classList.add("hidden"), 200);
 });
